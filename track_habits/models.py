69176c0f8509ed8a7e7dad0ff47c52f4d9b2e248
air <talko.alexander@gmail.com>
2024-09-03 09:39:47 +0400

    upd models

--- track_habits/models.py
+++ track_habits/models.py
@@ -12,19 +12,15 @@ class Habit(models.Model):
     place_habit = models.CharField(max_length=200, verbose_name='место, в котором необходимо выполнять привычку')
     time_habit = models.TimeField(auto_now=False, auto_now_add=False,
                                   verbose_name='время, когда необходимо выполнять привычку', help_text='HH:MM',)
-    action_habit = models.CharField(
-        max_length=350, verbose_name='действие, которое представляет собой привычка')
+    action_habit = models.CharField(max_length=350, verbose_name='действие, которое представляет собой привычка')
     is_pleasant = models.BooleanField(default=False, verbose_name='признак приятной привычки')
     related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Связанная привычка',
                                       **NULLABLE)
     reward = models.CharField(max_length=300, verbose_name='вознаграждение', **NULLABLE)
     duration = models.DurationField(verbose_name='время на выполнение привычки', **NULLABLE)
     is_public = models.BooleanField(default=False, verbose_name='признак публичности')
-    frequency = models.SmallIntegerField(
-        default=1,
-        verbose_name='Периодичность привычки (дней)',
-        help_text='Привычку выполнять каждые * дней',
-    )
+    frequency = models.SmallIntegerField(default=1, verbose_name='Периодичность привычки (дней)',
+                                         help_text='Привычку выполнять каждые * дней',)
 
     def __str__(self):
         return f'{self.owner_habit.email}: {self.action_habit}, {self.time_habit}, {self.place_habit}'
