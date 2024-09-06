5d64476efa5ccecd9b6df1c9e9a91ed77e1bee18
air <talko.alexander@gmail.com>
2024-09-06 11:40:37 +0400

    change

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'POSTGRES_HOST': os.getenv('POSTGRES_HOST'),
